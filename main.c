ab80a1164b316dec2fefa852849a0971c0f4da95
thomascharpentier <thomas.charpentier@viacesi.fr>
2016-11-04 16:12:31 +0100

    Add main program suite

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
-    printf("Hello world!\n");//bonjour
+    printf("%d\n", addition(1, 4));
 
 
 
