804049f4789922c844bf489fd055d189032102e5
thomascharpentier <thomas.charpentier@viacesi.fr>
2016-11-04 16:34:03 +0100

    j'ai fais le programme solo j'suis trop fort

--- main.c
+++ main.c
@@ -6,10 +6,17 @@ int addition(int a, int b)
     return a+b;
 }
 
+int soustraction(int c, int d)
+{
+    return c-d;
+}
+
 int main()
 {
 
     printf("a+b=%d\n", addition(1,4));
+    printf("c-d=%d\n", soustraction(6,2));
+
 
 
 
